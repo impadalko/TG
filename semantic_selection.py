4a6e1f2c311fb94d2a4f3c3bdb8e9d6e7ead2080
Ivan Padalko <ivan.monteiro.padalko@gmail.com>
2018-08-15 12:07:38 -0300

    Update to a more pythonish syntax

--- semantic_selection.py
+++ semantic_selection.py
@@ -2,13 +2,13 @@ from feature import Feature
 
 def similar_features(features, threshold):
     selected_features = []
-    for i in range(len(features)):
-        for j in range(len(selected_features)):
+    for feature in features:
+        for selected_feature in selected_features:
             # We use path similarity because we are looking for hypernyms
-            if features[i].path_similarity(selected_features[j]) > threshold:
+            if feature.path_similarity(selected_feature) > threshold:
                 break
         else:
             # If no similar enough feature is found among the already selected
             # ones, add the current synset to the current one
-            selected_features.append(features[i])
+            selected_features.append(feature)
     return selected_features
