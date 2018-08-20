bc5ba75751db13702fdcd47735f5af54033373ce
Ivan Padalko <ivan.monteiro.padalko@gmail.com>
2018-08-20 14:26:58 -0300

    Improve scraper

--- crawler.py
+++ crawler.py
@@ -7,54 +7,84 @@ from selenium.webdriver.firefox.options import Options
 
 from bs4 import BeautifulSoup
 
-base_url = 'https://www.ncbi.nlm.nih.gov'
-url = base_url + '/pubmed/?term=cancer'
+def main():
+    # Define URL to be scraped: the implementation of this web scraper is website
+    # dependent as we have a very specific objective and this should be hardcoded.
+    base_url = 'https://www.ncbi.nlm.nih.gov'
+    url = base_url + '/pubmed/?term=cancer'
 
-if len(sys.argv) < 2:
-    print('This script requires one argument: an integer representing the number' +
-            'of pages to be scraped')
-    sys.exit(1)
+    # Fails if wrong arguments are passed
+    # TODO: make this better: make the name of the file optional and add a help
+    if len(sys.argv) != 3:
+        print('This script requires exactly two arguments: the first being an ' +
+                'integer representing the number of pages to be scraped and the ' +
+                'second being the path of the output json file')
+        sys.exit(1)
 
-# Create a new Firefox headless session
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Firefox(firefox_options=options)
-driver.implicitly_wait(30)
-driver.get(url)
+    # Create a new Firefox headless session
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(firefox_options=options)
+    driver.implicitly_wait(30)
+    driver.get(url)
 
-# Change to sort by best match
-driver.find_element_by_css_selector('.relevancead_sort').click()
+    # Change to sort by best match
+    driver.find_element_by_css_selector('.relevancead_sort').click()
 
-number_of_pages = int(sys.argv[1])
+    # Article is defined as a set as there must be no repetition of links. In the
+    # case of repetition, we would have: slower performance (increase of requests)
+    # and a bigger influence of some articles over others.
+    articles = set()
 
-articles = set()
-for i in range(number_of_pages):
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    for link in soup.find_all('a', href=re.compile("^/pubmed/[0-9]+")):
-        articles.add(base_url + link['href'])
-    # Go to next page
-    driver.find_element_by_css_selector('.active.page_link.next').click()
+    number_of_pages = int(sys.argv[1])
+    print('Start reading website pages')
+    for i in range(number_of_pages):
+        print('Reading page', i+1)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        # Find all the links for articles in the page
+        for link in soup.find_all('a', href=re.compile("^/pubmed/[0-9]+")):
+            articles.add(base_url + link['href'])
+        # Go to next page
+        driver.find_element_by_css_selector('.active.page_link.next').click()
 
-parsed = []
-# Iterate over the chosen articles
-for article in articles:
-    driver.get(article)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    # Search the page for the desired infomation
-    title = soup.find_all('h1')[1].text
-    abstract = soup.find_all('div', class_='abstr')[0].text[8:]
-    keywords = soup.find('div', class_='keywords')
-    # Not all articles available have keywords
-    if keywords != None:
-        keywords = keywords.text[10:]
-        parsed.append({'title': title, 'abstract': abstract, 'keywords': keywords})
-    else:
-        parsed.append({'title': title, 'abstract': abstract})
+    print('Found', len(articles), 'articles')
 
-# Write results to an external file
-output_file = open('articles.json', 'w')
-output_file.write(json.dumps(parsed, sort_keys=True, indent=4))
-output_file.close()
+    parsed = []
+    print('Start processing articles')
+    i = 1
+    ignored = 0
+    # Iterate over the chosen articles
+    for article in articles:
+        print('Reading article', i, '-', article)
+        driver.get(article)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        # Search the page for the desired infomation
+        title = soup.find_all('h1')[1].text
+        abstract = soup.find('div', class_='abstr')
+        # Ignore articles without abstract
+        if abstract == None:
+            ignored += 1
+            continue
+        abstract = abstract.text[8:]
+        # Not all available articles have keywords
+        keywords = soup.find('div', class_='keywords')
+        if keywords != None:
+            keywords = keywords.text[10:]
+            parsed.append({'title': title, 'abstract': abstract, 'keywords': keywords})
+        else:
+            parsed.append({'title': title, 'abstract': abstract})
+        i += 1
 
-# Closes the session
-driver.quit()
+    print('Ignored', ignored, 'articles')
+
+    # Write results to an external file
+    print('Write results to output file')
+    output_file = open(sys.argv[2], 'w')
+    output_file.write(json.dumps(parsed, sort_keys=True, indent=4))
+    output_file.close()
+
+    # Closes the session
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
